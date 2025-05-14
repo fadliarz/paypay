d6e954a72d587f49c06f8d552541d2822a901944
fadliarz <fadliarz03@gmail.com>
2025-05-14 23:12:41 +0700

    feat: implement getter and builder pattern

--- order-service/order-domain/order-domain-core/src/main/java/com/paypay/order/service/domain/entity/OrderDetail.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/paypay/order/service/domain/entity/OrderDetail.java
@@ -10,4 +10,65 @@ public class OrderDetail {
   private UUID storeId;
   private List<OrderItem> orderItems;
   private BigDecimal price;
+
+  private OrderDetail(Builder builder) {
+    orderDetailId = builder.orderDetailId;
+    storeId = builder.storeId;
+    orderItems = builder.orderItems;
+    price = builder.price;
+  }
+
+  public UUID getOrderDetailId() {
+    return orderDetailId;
+  }
+
+  public UUID getStoreId() {
+    return storeId;
+  }
+
+  public List<OrderItem> getOrderItems() {
+    return orderItems;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public static final class Builder {
+
+    private UUID orderDetailId;
+    private UUID storeId;
+    private List<OrderItem> orderItems;
+    private BigDecimal price;
+
+    private Builder() {}
+
+    public static Builder builder() {
+      return new Builder();
+    }
+
+    public Builder orderDetailId(UUID val) {
+      orderDetailId = val;
+      return this;
+    }
+
+    public Builder storeId(UUID val) {
+      storeId = val;
+      return this;
+    }
+
+    public Builder orderItems(List<OrderItem> val) {
+      orderItems = val;
+      return this;
+    }
+
+    public Builder price(BigDecimal val) {
+      price = val;
+      return this;
+    }
+
+    public OrderDetail build() {
+      return new OrderDetail(this);
+    }
+  }
 }
