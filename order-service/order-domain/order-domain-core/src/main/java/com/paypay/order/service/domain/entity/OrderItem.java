8c78d18f36364ccf24bdf53e88df45679f3a1834
fadliarz <fadliarz03@gmail.com>
2025-05-14 23:19:29 +0700

    feat: implement getter and builder pattern

--- order-service/order-domain/order-domain-core/src/main/java/com/paypay/order/service/domain/entity/OrderItem.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/paypay/order/service/domain/entity/OrderItem.java
@@ -9,4 +9,65 @@ public class OrderItem {
   private Product product;
   private Integer quantity;
   private BigDecimal price;
+
+  private OrderItem(Builder builder) {
+    orderItemId = builder.orderItemId;
+    product = builder.product;
+    quantity = builder.quantity;
+    price = builder.price;
+  }
+
+  public UUID getOrderItemId() {
+    return orderItemId;
+  }
+
+  public Product getProduct() {
+    return product;
+  }
+
+  public Integer getQuantity() {
+    return quantity;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public static final class Builder {
+
+    private UUID orderItemId;
+    private Product product;
+    private Integer quantity;
+    private BigDecimal price;
+
+    private Builder() {}
+
+    public static Builder builder() {
+      return new Builder();
+    }
+
+    public Builder orderItemId(UUID val) {
+      orderItemId = val;
+      return this;
+    }
+
+    public Builder product(Product val) {
+      product = val;
+      return this;
+    }
+
+    public Builder quantity(Integer val) {
+      quantity = val;
+      return this;
+    }
+
+    public Builder price(BigDecimal val) {
+      price = val;
+      return this;
+    }
+
+    public OrderItem build() {
+      return new OrderItem(this);
+    }
+  }
 }
