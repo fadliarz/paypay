c2b5fa9e77b29be817c98817349b59064e9c1926
fadliarz <fadliarz03@gmail.com>
2025-05-29 05:11:21 +0700

    feat: create BeanConfiguration

---
+++ order-service/order-container/src/main/java/com/paypay/order/service/domain/BeanConfiguration.java
@@ -0,0 +1,12 @@
+package com.paypay.order.service.domain;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+  @Bean
+  public OrderDomainService orderDomainService() {
+    return new OrderDomainServiceImpl();
+  }
+}
