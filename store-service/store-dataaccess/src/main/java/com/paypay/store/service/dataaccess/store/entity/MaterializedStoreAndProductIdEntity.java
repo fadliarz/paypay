342a4253b17398dd1f1f95342589a8a975540224
fadliarz <fadliarz03@gmail.com>
2025-05-30 01:00:38 +0700

    feat: implement equals() and hashCode()

--- store-service/store-dataaccess/src/main/java/com/paypay/store/service/dataaccess/store/entity/MaterializedStoreAndProductIdEntity.java
+++ store-service/store-dataaccess/src/main/java/com/paypay/store/service/dataaccess/store/entity/MaterializedStoreAndProductIdEntity.java
@@ -1,6 +1,7 @@
 package com.paypay.store.service.dataaccess.store.entity;
 
 import java.io.Serializable;
+import java.util.Objects;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,4 +18,17 @@ public class MaterializedStoreAndProductIdEntity implements Serializable {
 
   private UUID storeId;
   private UUID productId;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    MaterializedStoreAndProductIdEntity that = (MaterializedStoreAndProductIdEntity) o;
+    return Objects.equals(storeId, that.storeId) && Objects.equals(productId, that.productId);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(storeId, productId);
+  }
 }
