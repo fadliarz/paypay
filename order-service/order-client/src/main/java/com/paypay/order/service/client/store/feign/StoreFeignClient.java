713968e1dc441073577c11327f0776b6e657785f
fadliarz <fadliarz03@gmail.com>
2025-05-30 05:59:21 +0700

    feat: add hardcoded url

--- order-service/order-client/src/main/java/com/paypay/order/service/client/store/feign/StoreFeignClient.java
+++ order-service/order-client/src/main/java/com/paypay/order/service/client/store/feign/StoreFeignClient.java
@@ -5,13 +5,11 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Component
-@FeignClient(name = "store-service")
+@FeignClient(name = "store-service", url = "http://localhost:9121")
 public interface StoreFeignClient {
 
   @GetMapping("/stores/{storeId}/products")
