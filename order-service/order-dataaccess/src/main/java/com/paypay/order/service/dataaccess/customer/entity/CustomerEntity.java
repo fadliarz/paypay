dc554fe95387d9ef0406176d3abcadba4734959b
fadliarz <fadliarz03@gmail.com>
2025-05-30 05:59:54 +0700

    feat: add schema name

--- order-service/order-dataaccess/src/main/java/com/paypay/order/service/dataaccess/customer/entity/CustomerEntity.java
+++ order-service/order-dataaccess/src/main/java/com/paypay/order/service/dataaccess/customer/entity/CustomerEntity.java
@@ -3,12 +3,12 @@ package com.paypay.order.service.dataaccess.customer.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import java.util.UUID;
 
 @Builder
 @NoArgsConstructor
@@ -16,7 +16,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @Entity
-@Table(name = "customers")
+@Table(name = "customers", schema = "\"order\"")
 public class CustomerEntity {
 
   @Id private UUID id;
