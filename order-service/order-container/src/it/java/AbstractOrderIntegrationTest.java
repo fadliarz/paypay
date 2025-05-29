a8494c842b73d88a125d65a4fcd61df04927a651
fadliarz <fadliarz03@gmail.com>
2025-05-30 06:00:37 +0700

    feat: implement WireMock and add more test cases

--- order-service/order-container/src/it/java/AbstractOrderIntegrationTest.java
+++ order-service/order-container/src/it/java/AbstractOrderIntegrationTest.java
@@ -1,32 +1,51 @@
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.paypay.order.service.client.store.entity.ProductDetails;
+import com.paypay.order.service.client.store.entity.StoreDetails;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.UUID;
 import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.TestcontainersExtension;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 import org.testcontainers.utility.DockerImageName;
 
 @ExtendWith(TestcontainersExtension.class)
 @Slf4j
 public class AbstractOrderIntegrationTest {
-
   protected static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER;
-  protected static DataSource DATA_SOURCE;
+  protected static final String ORDER_SCHEMA = "order";
+
+  protected static final ObjectMapper objectMapper = new ObjectMapper();
+  protected static WireMockServer wireMockServer;
+  protected static DataSource dataSource;
+
+  protected static UUID mockedStoreId;
+  protected static StoreDetails mockedStoreDetails;
+  protected static List<ProductDetails> mockedProducts;
+  protected static ProductDetails mockedFirstProductDetails;
+  protected static ProductDetails mockedSecondProductDetails;
 
   static {
-    try {
-      Class.forName("org.postgresql.Driver");
-    } catch (ClassNotFoundException classNotFoundException) {
-      throw new RuntimeException("Critical: PostgreSQL driver not found", classNotFoundException);
-    }
+    wireMockServer = new WireMockServer(9121);
 
     POSTGRE_SQL_CONTAINER =
         new PostgreSQLContainer<>(DockerImageName.parse("postgres:15.2-alpine"))
@@ -37,48 +56,35 @@ public class AbstractOrderIntegrationTest {
     POSTGRE_SQL_CONTAINER.start();
     log.info("PostgreSQL container started at {}", POSTGRE_SQL_CONTAINER.getJdbcUrl());
 
-    try (Connection c =
-        java.sql.DriverManager.getConnection(
-            POSTGRE_SQL_CONTAINER.getJdbcUrl(),
-            POSTGRE_SQL_CONTAINER.getUsername(),
-            POSTGRE_SQL_CONTAINER.getPassword())) {
-    } catch (SQLException sqlException) {
-      throw new RuntimeException(
-          "Critical: Could not connect to Testcontainer directly vid DriverManager", sqlException);
-    }
-
     initializeDataSource();
     applyMigrations();
   }
 
-  private static void initializeDataSource() {
+  protected static void initializeDataSource() {
     HikariConfig hikariConfig = new HikariConfig();
     hikariConfig.setJdbcUrl(POSTGRE_SQL_CONTAINER.getJdbcUrl());
-    hikariConfig.setSchema("order");
+    hikariConfig.addDataSourceProperty("currentSchema", ORDER_SCHEMA);
     hikariConfig.setUsername(POSTGRE_SQL_CONTAINER.getUsername());
     hikariConfig.setPassword(POSTGRE_SQL_CONTAINER.getPassword());
     hikariConfig.setDriverClassName(POSTGRE_SQL_CONTAINER.getDriverClassName());
     hikariConfig.setMaximumPoolSize(5);
-    DATA_SOURCE = new HikariDataSource(hikariConfig);
+    dataSource = new HikariDataSource(hikariConfig);
     System.out.println("Hikari DataSource initialized.");
   }
 
-  private static void applyMigrations() {
+  protected static void applyMigrations() {
     Flyway flyway =
         Flyway.configure()
-            .dataSource(
-                POSTGRE_SQL_CONTAINER.getJdbcUrl(),
-                POSTGRE_SQL_CONTAINER.getUsername(),
-                POSTGRE_SQL_CONTAINER.getPassword())
+            .dataSource(dataSource)
             .locations("classpath:migration")
-            .schemas("order")
+            .schemas(ORDER_SCHEMA)
             .load();
     flyway.migrate();
     log.info("Flyway migrations applied successfully");
   }
 
   @DynamicPropertySource
-  private static void registerPgProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+  protected static void registerPgProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
     log.info("Registering postgres properties...");
     dynamicPropertyRegistry.add("spring.datasource.url", POSTGRE_SQL_CONTAINER::getJdbcUrl);
     dynamicPropertyRegistry.add("spring.datasource.username", POSTGRE_SQL_CONTAINER::getUsername);
@@ -87,10 +93,39 @@ public class AbstractOrderIntegrationTest {
     log.info("Registered postgres properties");
   }
 
+  @BeforeAll
+  public static void setupMockedObject() {
+    mockedStoreId = UUID.randomUUID();
+    mockedFirstProductDetails =
+        ProductDetails.builder()
+            .id(UUID.randomUUID())
+            .image("image")
+            .name("name")
+            .description("description")
+            .price(BigDecimal.valueOf(5))
+            .build();
+    mockedSecondProductDetails =
+        ProductDetails.builder()
+            .id(UUID.randomUUID())
+            .image("image")
+            .name("name")
+            .description("description")
+            .price(BigDecimal.valueOf(5))
+            .build();
+    mockedProducts = List.of(mockedFirstProductDetails, mockedSecondProductDetails);
+    mockedStoreDetails = StoreDetails.builder().id(mockedStoreId).products(mockedProducts).build();
+  }
+
+  @BeforeEach
+  void startWireMockServer() {
+    wireMockServer.start();
+    log.info("Started WireMock server");
+  }
+
   @BeforeEach
   void cleanTablesBetweenTests() {
     String[] tables = {"orders", "order_items"};
-    try (Connection connection = DATA_SOURCE.getConnection()) {
+    try (Connection connection = dataSource.getConnection()) {
       for (String table : tables) {
         try (PreparedStatement preparedStatement =
             connection.prepareStatement("TRUNCATE TABLE " + table + " RESTART IDENTITY CASCADE")) {
@@ -101,5 +136,32 @@ public class AbstractOrderIntegrationTest {
     } catch (SQLException sqlException) {
       throw new RuntimeException("Failed to clean tables!", sqlException);
     }
+  }
+
+  @AfterEach
+  void stopWireMockServer() {
+    wireMockServer.stop();
+    log.info("Stopped WireMock server");
+  }
+
+  protected void mockFindStoreWithProducts200Ok() {
+    try {
+      wireMockServer.stubFor(
+          get(urlPathEqualTo("/stores/" + mockedStoreId + "/products"))
+              .willReturn(
+                  aResponse()
+                      .withStatus(200)
+                      .withHeader("Content-Type", "application/json")
+                      .withBody(objectMapper.writeValueAsString(mockedStoreDetails))));
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  protected void mockFindStoreWithProduct404NotFound() {
+    wireMockServer.stubFor(
+        get(urlPathEqualTo("/stores/" + mockedStoreId + "/products"))
+            .willReturn(
+                aResponse().withStatus(404).withHeader("Content-Type", "application/json")));
   }
 }
--- order-service/order-container/src/it/java/CreateOrderIntegrationTest.java
+++ order-service/order-container/src/it/java/CreateOrderIntegrationTest.java
@@ -1,8 +1,14 @@
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.paypay.order.service.domain.OrderServiceApplication;
+import com.paypay.order.service.domain.entity.Store;
+import com.paypay.order.service.domain.exception.CustomerNotFoundException;
+import com.paypay.order.service.domain.exception.StoreNotFoundException;
 import com.paypay.order.service.domain.features.create.order.CreateOrderCommandHandler;
+import com.paypay.order.service.domain.ports.output.client.StoreClient;
+import command.CommandFactory;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,9 +19,37 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 public class CreateOrderIntegrationTest extends AbstractOrderIntegrationTest {
 
   @Autowired private CreateOrderCommandHandler createOrderCommandHandler;
+  @Autowired private StoreClient storeClient;
 
   @Test
   void placeOrder_whenCommandIsValid_persistOrderAndItems() {
-    assertEquals(1, 1);
+    mockFindStoreWithProducts200Ok();
+
+    Optional<Store> optionalStore =
+        storeClient.findStoreInformationWithProducts(
+            mockedStoreId,
+            mockedProducts.stream().map(mockedProduct -> mockedProduct.getId()).toList());
+
+    assertFalse(optionalStore.isEmpty());
+    Store store = optionalStore.get();
+    assertNotNull(store);
+    assertEquals(mockedStoreDetails.getId(), store.getId());
+    assertEquals(mockedStoreDetails.getProducts().size(), store.getProducts().size());
+  }
+
+  @Test
+  void placeOrder_whenStoreNotFound_throwStoreNotFoundException() {
+    mockFindStoreWithProduct404NotFound();
+
+    assertThrows(
+        StoreNotFoundException.class,
+        () -> createOrderCommandHandler.handle(CommandFactory.newCreateOrderCommand()));
+  }
+
+  @Test
+  void placeOrder_whenCustomerNotFound__throwCustomerNotFoundException() {
+    assertThrows(
+        CustomerNotFoundException.class,
+        () -> createOrderCommandHandler.handle(CommandFactory.newCreateOrderCommand()));
   }
 }
