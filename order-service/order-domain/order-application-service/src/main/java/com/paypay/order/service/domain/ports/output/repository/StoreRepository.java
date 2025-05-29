ac2e01b37024d68ce1c03267332cc1d596bff0d6
fadliarz <fadliarz03@gmail.com>
2025-05-29 19:42:35 +0700

    chore: delete store repository implementation

--- order-service/order-domain/order-application-service/src/main/java/com/paypay/order/service/domain/ports/output/repository/StoreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.paypay.order.service.domain.ports.output.repository;
-
-import com.paypay.order.service.domain.entity.Store;
-import java.util.Optional;
-
-public interface StoreRepository {
-
-  Optional<Store> findStoreInformation(Store store);
-}
