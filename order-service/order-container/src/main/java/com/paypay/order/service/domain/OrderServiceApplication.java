b6bb3f1b6f3ff9ba6520c6457cb050a20728db9c
fadliarz <fadliarz03@gmail.com>
2025-05-30 05:59:11 +0700

    feat: enable feign clients

--- order-service/order-container/src/main/java/com/paypay/order/service/domain/OrderServiceApplication.java
+++ order-service/order-container/src/main/java/com/paypay/order/service/domain/OrderServiceApplication.java
@@ -3,12 +3,14 @@ package com.paypay.order.service.domain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(
     basePackages = {"com.paypay.order.service.dataaccess", "com.paypay.dataaccess"})
 @EntityScan(basePackages = {"com.paypay.order.service.dataaccess", "com.paypay.dataaccess"})
 @SpringBootApplication(scanBasePackages = "com.paypay")
+@EnableFeignClients(basePackages = {"com.paypay.order.service.client"})
 public class OrderServiceApplication {
 
   public static void main(String[] args) {
