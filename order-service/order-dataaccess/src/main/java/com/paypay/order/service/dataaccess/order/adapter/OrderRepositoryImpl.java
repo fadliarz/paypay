dce5ff01afee39b6a10f2f6ea6abd32272307aae
fadliarz <fadliarz03@gmail.com>
2025-05-31 17:52:50 +0700

    feat: implement find order

--- order-service/order-dataaccess/src/main/java/com/paypay/order/service/dataaccess/order/adapter/OrderRepositoryImpl.java
+++ order-service/order-dataaccess/src/main/java/com/paypay/order/service/dataaccess/order/adapter/OrderRepositoryImpl.java
@@ -4,7 +4,10 @@ import com.paypay.order.service.dataaccess.order.mapper.OrderDataAccessMapper;
 import com.paypay.order.service.dataaccess.order.repository.OrderJpaRepository;
 import com.paypay.order.service.domain.entity.Order;
 import com.paypay.order.service.domain.ports.output.repository.OrderRepository;
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class OrderRepositoryImpl implements OrderRepository {
@@ -22,5 +25,11 @@ public class OrderRepositoryImpl implements OrderRepository {
   public Order save(Order order) {
     return orderDataAccessMapper.orderEntityToOrder(
         orderJpaRepository.save(orderDataAccessMapper.orderToOrderEntity(order)));
+  }
+
+  @Override
+  @Transactional
+  public Optional<Order> findOrder(UUID orderId) {
+    return orderJpaRepository.findById(orderId).map(orderDataAccessMapper::orderEntityToOrder);
   }
 }
--- order-service/order-domain/order-application-service/src/main/java/com/paypay/order/service/domain/ports/output/repository/OrderRepository.java
+++ order-service/order-domain/order-application-service/src/main/java/com/paypay/order/service/domain/ports/output/repository/OrderRepository.java
@@ -1,8 +1,12 @@
 package com.paypay.order.service.domain.ports.output.repository;
 
 import com.paypay.order.service.domain.entity.Order;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface OrderRepository {
 
   Order save(Order order);
+
+  Optional<Order> findOrder(UUID orderId);
 }
