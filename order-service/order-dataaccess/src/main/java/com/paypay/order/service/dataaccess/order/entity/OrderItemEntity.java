09e431f2b0b0d4d18b14fc5d4c281e8d5e3338d6
fadliarz <fadliarz03@gmail.com>
2025-05-31 17:52:13 +0700

    feat: specify schema

--- order-service/order-dataaccess/src/main/java/com/paypay/order/service/dataaccess/order/entity/OrderItemEntity.java
+++ order-service/order-dataaccess/src/main/java/com/paypay/order/service/dataaccess/order/entity/OrderItemEntity.java
@@ -21,7 +21,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "order_items")
+@Table(schema = "order", name = "order_items")
 @IdClass(OrderItemIdEntity.class)
 public class OrderItemEntity {
 
