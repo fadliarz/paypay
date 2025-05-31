8857821686e9810d792fde626d2dd87500e716c8
fadliarz <fadliarz03@gmail.com>
2025-05-31 17:55:26 +0700

    feat: implement new test case

--- order-service/order-container/src/it/java/com/paypay/order/service/domain/CreateOrderIntegrationTest.java
+++ order-service/order-container/src/it/java/com/paypay/order/service/domain/CreateOrderIntegrationTest.java
@@ -2,39 +2,48 @@ package com.paypay.order.service.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.paypay.order.service.domain.command.CommandFactory;
-import com.paypay.order.service.domain.entity.Store;
+import com.paypay.order.service.domain.entity.Order;
 import com.paypay.order.service.domain.exception.CustomerNotFoundException;
 import com.paypay.order.service.domain.exception.StoreNotFoundException;
 import com.paypay.order.service.domain.features.create.order.CreateOrderCommandHandler;
-import com.paypay.order.service.domain.ports.output.client.StoreClient;
+import com.paypay.order.service.domain.ports.output.repository.OrderRepository;
+import java.math.BigDecimal;
 import java.util.Optional;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest(classes = {OrderServiceApplication.class})
 @Testcontainers
 public class CreateOrderIntegrationTest extends AbstractOrderIntegrationTest {
 
   @Autowired private CreateOrderCommandHandler createOrderCommandHandler;
-  @Autowired private StoreClient storeClient;
+  @Autowired private OrderRepository orderRepository;
 
   @Test
   void placeOrder_whenCommandIsValid_persistOrderAndItems() {
     mockFindStoreWithProducts200Ok();
 
-    Optional<Store> optionalStore =
-        storeClient.findStoreInformationWithProducts(
-            mockedStoreId,
-            mockedProducts.stream().map(mockedProduct -> mockedProduct.getId()).toList());
+    Order orderResult = createOrderCommandHandler.handle(createOrderCommand).getOrder();
 
-    assertFalse(optionalStore.isEmpty());
-    Store store = optionalStore.get();
-    assertNotNull(store);
-    assertEquals(mockedStoreDetails.getId(), store.getId());
-    assertEquals(mockedStoreDetails.getProducts().size(), store.getProducts().size());
+    assertNotNull(orderResult);
+    assertNotNull(orderResult.getId());
+    Optional<Order> optionalSavedOrder = orderRepository.findOrder(orderResult.getId());
+    assertFalse(optionalSavedOrder.isEmpty());
+    Order savedOrder = optionalSavedOrder.get();
+    assertNotNull(savedOrder);
+    assertEquals(savedOrder, savedOrder);
+    assertNotNull(orderResult);
+  }
+
+  @Test
+  void placeOrder_whenTotalPriceIsInvalid_throwError() {
+    mockFindStoreWithProducts200Ok();
+    createOrderCommandBuilder.totalPrice(createOrderCommand.getTotalPrice().add(BigDecimal.ONE));
+
+    assertThrows(
+        RuntimeException.class,
+        () -> createOrderCommandHandler.handle(createOrderCommandBuilder.build()));
   }
 
   @Test
@@ -48,8 +57,10 @@ public class CreateOrderIntegrationTest extends AbstractOrderIntegrationTest {
 
   @Test
   void placeOrder_whenCustomerNotFound__throwCustomerNotFoundException() {
+    createOrderCommandBuilder.customerId(UUID.randomUUID());
+
     assertThrows(
         CustomerNotFoundException.class,
-        () -> createOrderCommandHandler.handle(CommandFactory.newCreateOrderCommand()));
+        () -> createOrderCommandHandler.handle(createOrderCommandBuilder.build()));
   }
 }
