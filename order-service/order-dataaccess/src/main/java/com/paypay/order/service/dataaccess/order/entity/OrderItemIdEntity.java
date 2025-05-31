0dff8633ae118af0f4d548f2ce18ed0b2f59b0ac
fadliarz <fadliarz03@gmail.com>
2025-05-31 17:52:36 +0700

    feat: add Builder annotation

--- order-service/order-dataaccess/src/main/java/com/paypay/order/service/dataaccess/order/entity/OrderItemIdEntity.java
+++ order-service/order-dataaccess/src/main/java/com/paypay/order/service/dataaccess/order/entity/OrderItemIdEntity.java
@@ -2,10 +2,12 @@ package com.paypay.order.service.dataaccess.order.entity;
 
 import java.io.Serializable;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
